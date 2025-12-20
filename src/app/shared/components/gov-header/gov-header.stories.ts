3759511b5aea4d0b91e02bd153f3e4bbd094b375
asb3 <asb3@g2c46.cin.ufpe.br>
2025-12-20 19:41:51 -0300

    storybooking concluido

---
+++ src/app/shared/components/gov-header/gov-header.stories.ts
@@ -0,0 +1,33 @@
+import type { Meta, StoryObj } from '@storybook/angular';
+import { applicationConfig } from '@storybook/angular';
+import { GovHeader } from './gov-header';
+import { provideRouter } from '@angular/router';
+import { AccessibilityService } from '../../../core/services/accessibility.service';
+
+const meta: Meta<GovHeader> = {
+  title: 'Componentes/GovHeader',
+  component: GovHeader,
+  tags: ['autodocs'],
+  decorators: [
+    applicationConfig({
+      providers: [
+        provideRouter([]),
+        AccessibilityService
+      ],
+    }),
+  ],
+};
+
+export default meta;
+type Story = StoryObj<GovHeader>;
+
+export const Padrao: Story = {};
+
+export const AltoContraste: Story = {
+  parameters: {
+    backgrounds: { default: 'dark' },
+  },
+  play: async () => {
+    // Aqui poderíamos simular o clique no botão de contraste
+  }
+};
